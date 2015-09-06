412440360b23aca7a693af0ad3cc492172b0aaab
Frank Cheng <ufo22940268@gmail.com>
2015-09-07 07:45:10 +0800

    up

--- src/main/groovy/org/gradle/GreetingTask.groovy
+++ src/main/groovy/org/gradle/GreetingTask.groovy
@@ -6,10 +6,11 @@ import org.gradle.api.tasks.TaskAction
 class GreetingTask extends DefaultTask {
 
     String channelFile
+    String outFile
 
     @TaskAction
     def greet() {
-        def channels = project.file(channelFile).text.split().findAll({str -> str?.trim()});
-        println channels
+        def channels = project.file(channelFile).text.split().findAll({str -> str?.trim()})
+        Client.startPackTask(outFile, channels);
     }
 }
